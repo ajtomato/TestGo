f7bce6fc7ca0273240c1b7044b0ab3cae89dd6b7
ajtomato <ajtomato@gmail.com>
2017-05-26 15:17:28 +0800

    var declaration

--- hello/hello.go
+++ hello/hello.go
@@ -6,6 +6,14 @@ import (
 	"ajtomato.gmail.com/test/stringutil"
 )
 
+// A var declaration can include initializers, one per variable.
+var a, b int = 1, 2
+
 func main() {
+	// If an initializer is present, the type can be omitted. Please note that
+	// c, python, java have different types.
+	var c, python, java = true, false, "no!"
+
 	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	fmt.Printf("%v, %v, %v", c, python, java)
 }

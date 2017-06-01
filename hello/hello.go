a677878800c0458d67132d353c6587318f02c91d
ajtomato <ajtomato@gmail.com>
2017-06-01 16:56:19 +0800

    close channel & range

--- hello/hello.go
+++ hello/hello.go
@@ -345,6 +345,28 @@ func testBufferedChannel() {
 	fmt.Println(<-ch)
 }
 
+func fibonacci(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+	// Only the sender should close a channel
+	close(c)
+}
+
+func closeChannel() {
+	c := make(chan int, 10)
+	go fibonacci(cap(c), c)
+	// The loop for i := range c receives values from the channel repeatedly
+	// until it is closed.
+	// v, ok := <-c; ok is false if there are no more values to receive and
+	// the channel is closed.
+	for i := range c {
+		fmt.Println(i)
+	}
+}
+
 func main() {
 	// If an initializer is present, the type can be omitted. Please note that
 	// c, python, java have different types.
@@ -356,5 +378,5 @@ func main() {
 	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
 	fmt.Printf("%v, %v, %v, %v, %v\n", c, python, java, e, d)
 
-	testBufferedChannel()
+	closeChannel()
 }
